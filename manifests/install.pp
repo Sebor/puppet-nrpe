0978633e8ffb761692b0eb92a5f98d5c6f08b25f
Sebor <Sebor@users.noreply.github.com>
2018-06-01 09:59:40 +0300

    Fix variable inherits

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,6 @@
 class nrpe::install (
-  $package_ensure = $nrpe::package_ensure,
-  $nrpe_pkgs      = $nrpe::nrpe_pkgs,
+  $package_ensure = $nrpe::params::package_ensure,
+  $nrpe_pkgs      = $nrpe::params::nrpe_pkgs,
 ) inherits nrpe {
 
   package { $nrpe_pkgs:
