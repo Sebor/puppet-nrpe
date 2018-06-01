be08bf0a218585d20eba62fb58e72233056224f3
Sebor <Sebor@users.noreply.github.com>
2018-06-01 10:00:25 +0300

    Fix variable inherits

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,8 +1,8 @@
 class nrpe::service (
-  $service_enable = $nrpe::service_enable,
-  $service_ensure = $nrpe::service_ensure,
-  $service_manage = $nrpe::service_manage,
-  $nrpe_service   = $nrpe::nrpe_service,
+  $service_enable = $nrpe::params::service_enable,
+  $service_ensure = $nrpe::params::service_ensure,
+  $service_manage = $nrpe::params::service_manage,
+  $nrpe_service   = $nrpe::params::nrpe_service,
 ) inherits nrpe {
 
   if ! ($service_ensure in [ 'running', 'stopped' ]) {
